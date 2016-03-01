aa9c9d27e4b15b07fc8c88a18bb831ee8b6428b6
anplamon <anplamon@ualberta.ca>
2016-03-01 10:56:57 -0700

    Have an implementation of the parallel sort.

--- multisort/lib/multisort/sorter.rb
+++ multisort/lib/multisort/sorter.rb
@@ -1,3 +1,5 @@
+require “thread”
+
 module Multisort
   module Sorter
 
@@ -9,14 +11,32 @@ module Multisort
       #           data_cleaned = true
       #           sort_status = false
       #           time_limit
+      $semaphore = Mutex.new
+      
+      mainBucket = Array.new()
+      build_buckets(data)
+      sortingThread = Thread.new{thread_handler(data)}
+      watchdogThread = Thread.new{watchdog(time)}
+      
+      sortingThread.abort_on_exception = true
+      sortingThread.join
+      watchdog.join
+      
+      if sortingThread == "dead"
+          Thread.list.each{|t| Thread.kill(t)}
+      end
     end
 
     # Contract
-    def watchdog
+    def watchdog(time)
       # task timer
       # ends process if watchdog value is passed
       # watchdog value represented in seconds
       # pre       time_limit
+      sleep time
+      if sortingThread.alive?
+          Thread.kill(sortingThread)
+      end
     end
 
     # Contract
@@ -25,28 +45,101 @@ module Multisort
       # each initial bucket has 2 data points
       # pre       data loaded
       # post      bucket containing data
+      dataclone = data.clone
+      while dataclone.length > 1 do
+          mainBucket.push(dataclone.shift(2))
+      end
+      
+      # Handles the exception if there is an odd number of values in the dataset.
+      if dataclone.length == 1
+          mainBucket.pushArray.new(dataclone.shift))
+      end
     end
 
     def thread_handler(data)
       # given initial data input, handles thread operations with thread_sort and combine_bucket
       # pre       data
       # post      completed thread operation
+      
+      while mainBuckets.count != 1
+        # Sort sub buckets in main bucket.
+        threads = Array.new
+        (0..(mainBucket.count-1)).each do |rank|
+          threads << Thread.new do
+            thread_sort(rank)
+          end
+        end
+                  
+        # ref http://stackoverflow.com/questions/9095316/handling-exceptions-raised-in-a-ruby-thread
+        threads.each(&:join)
+      
+        # combine sub buckets
+        threads = Array.new
+        threadBuckets = Array.new()
+        (0..(mainBucket.count/2).floor-1).each do |rank|
+          threads << Thread.new do
+            combine_bucket(rank)
+          end
+        end
+      
+        threads.each(&:join)
+        mainBucket = threadBuckets
+      end
     end
 
     # Contract
-    def thread_sort
+    def thread_sort(rank)
       # thread sort operations parsed here
       # Exceptions: thread-based exceptions
       # pre       data
       # post      sorted data
+    
+      # sort sub bucket.
+      subBucket = mainBucket[rank]
+      quick_sort(mainBucket[rank])
+    
+      # write bucket back to mainBucket
+      $semaphore.lock
+        mainBucket[rank] = subBucket
+      $semaphore.unlock
     end
 
     # Contract
-    def combine_bucket(thread)
+    def combine_bucket(rank)
       # combines bucket with another bucket
       # combines data, then performs thread_sort again
       # pre       two sorted buckets
       # post      single sorted bucket
+      
+      # combine buckets
+      combinedBucket = merge_sorted_arrays(mainBucket[2*rank], mainBucket[(2*rank)+1])
+      
+      # Write combined bucket to bucket list
+      $semaphore.lock
+        threadBuckets.push(combinedBucket)
+      $semaphore.unlock
+    end
+    
+    def quick_sort(array)
+        sl = array.clone
+        return sl if sl.size <= 1
+        pivot = sl.pop
+        left, right = sl.partition { |e| e < pivot }
+        quick_sort(left) + [pivot] + quick_sort(right)
+    end
+    
+    def merge_sorted_arrays(array1, array2)
+      combinedArray = Array.new
+      
+      while !array1.empty? || !array2.empty?
+        if array1.first >= array2.first
+          combinedArray.push(array1.shift)
+        else
+          combinedArray.push(array1.shift)
+        end
+      end
+      
+      return combinedArray + array1 + array2
     end
 
   end # Sorter
