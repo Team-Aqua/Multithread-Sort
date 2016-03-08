75abd4ba3ec1e79c9775abcb23e16b7bc9cf362e
Quentin Lautischer <lautisch@ualberta.ca>
2016-03-08 13:39:40 -0700

    tests

--- multisort/test/sorter_test.rb
+++ multisort/test/sorter_test.rb
@@ -8,20 +8,47 @@ class SorterTest < Minitest::Test
   def setup
   end
   
-  def test_main_sort
+  def test_sort_int
     @ms = Multisort::MSDriver.new
     @ms.load_from_data([2, 4, 6, 8, 10, 1, 3, 5, 7, 9])
     assert_equal(true, @ms.data_loaded)
     @ms.main_sort
     assert_equal(true, @ms.data == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
+    @ms.load_from_data([10, 1])
+    assert_equal(true, @ms.data_loaded)
+    @ms.main_sort
+    assert_equal(true, @ms.data == [1, 10])
 
-    # assert_output("[1, 2, 3, 4, 5, 6, 7, 8, 9]") {main_sort([2, 7, 1, 8, 6, 3, 5, 4, 9], 2)}
-    # assert_output("[1, 2]") {main_sort([2, 1], 1)}
-    # assert_output("[1]") {main_sort([1], 1)}
-    # #assert_output("['Hello', 'World', 'From', 'Aaron']") {main_sort(["Aaron", "World", "Hello", "From"], 2)}
+    @ms.load_from_data([1])
+    assert_equal(true, @ms.data_loaded)
+    @ms.main_sort
+    assert_equal(true, @ms.data == [1])
+  end
+
+  def test_sort_string
+    @ms = Multisort::MSDriver.new
+    @ms.load_from_data(["Anson", "Quentin", "Aaron"])
+    assert_equal(true, @ms.data_loaded)
+    @ms.main_sort
+    assert_equal(true, @ms.data == ["Aaron", "Anson", "Quentin"])
+  end
+
+  def test_benchmarks
+    # @ms = Multisort::MSDriver.new
+    # @ms.load_from_file("test/test_files/extra_large_csv.csv")
+    # assert_equal(true, @ms.data_loaded)
+    # timer {@ms.main_sort}
+    # @ms.load_from_file("test/test_files/extra_large_csv.csv")
+    # timer {@ms.bubble_sort(@ms.data)}
   end
   
+  def timer
+    t = Time.now
+    result = yield
+    puts "\nCompleted in #{Time.now - t} seconds"
+    result
+  end
   # def test_failed_main_sort
   #   assert_output("Return error") {main_sort([2, 4, 6, 8, 10, 1, 3, 5, 7, 9], 0)}
   # end
