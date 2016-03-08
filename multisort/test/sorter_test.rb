1ca1badc719846b3509e3e573ba941aab2d14dcc
Quentin Lautischer <lautisch@ualberta.ca>
2016-03-08 12:49:09 -0700

    save

--- multisort/test/sorter_test.rb
+++ multisort/test/sorter_test.rb
@@ -9,7 +9,7 @@ class SorterTest < Minitest::Test
   end
   
   def test_main_sort
-      assert_output("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]") {main_sort([2, 4, 6, 8, 10, 1, 3, 5, 7, 9], 2)}
+      assert_output([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) {main_sort([2, 4, 6, 8, 10, 1, 3, 5, 7, 9], 2)}
       assert_output("[1, 2, 3, 4, 5, 6, 7, 8, 9]") {main_sort([2, 7, 1, 8, 6, 3, 5, 4, 9], 2)}
       assert_output("[1, 2]") {main_sort([2, 1], 1)}
       assert_output("[1]") {main_sort([1], 1)}
