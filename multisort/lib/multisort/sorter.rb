ab3bc5f8ed91028530917d33b47a1fe6ecb32b1f
Quentin Lautischer <lautisch@ualberta.ca>
2016-03-08 17:08:41 -0700

    re-formated sorter.rb for modular calls

--- multisort/lib/multisort/sorter.rb
+++ multisort/lib/multisort/sorter.rb
@@ -2,159 +2,156 @@ module Multisort
   module Sorter
     include Contracts::Core
     C = Contracts
-    
+
     Thread::abort_on_exception = true
-    #Contract C::And[MContracts::SortData] => C::None
-    def main_sort
-      # main sort of process
+
+    def self.main_sort(data, threadNum, timer)
+      puts threadNum
       # delegation and tasks presented here
-      # pre       data_loaded = true
-      #           data_cleaned = true
-      #           sort_status = false
-      #           time_limit
       $semaphore = Mutex.new
       
-      @mainBucket = Array.new()
-      if @data.count > 2
-        build_buckets
+      $mainBucket = Array.new()
+      if data.count > 2
+        build_buckets(data, threadNum)
       else
-        @data = bubble_sort(@data)
-        @sort_status = true
-        return
+        data = bubble_sort(data)
+        return data
       end
       
-      begin
-        @sortingThread = Thread.new{thread_handler}
-      rescue Exception => e
-        puts "EXCEPTION: #{e.inspect}"
-        puts "MESSAGE: #{e.message}"
-      end
-      #@sortingThread.abort_on_exception = true
-      watchdogThread = Thread.new{watchdog}
+      # begin
+      $sortingThread = Thread.new{thread_handler}
+      # rescue Exception => e
+      #   puts "EXCEPTION: #{e.inspect}"
+      #   puts "MESSAGE: #{e.message}"
+      # end
+      #$sortingThread.abort_on_exception = true
+      watchdogThread = Thread.new{watchdog(timer)}
       
-      @sortingThread.join
+      $sortingThread.join
       
       if watchdogThread.alive?
           Thread.kill(watchdogThread)
       end
       watchdogThread.join
       
-      puts @sortingThread.status
-      if @sortingThread.status ==  "aborting"
-          puts "The thread was killed and caught"
-          Thread.list.each{|t| Thread.kill(t)}
-      end
+      # puts $sortingThread.status
+      # if $sortingThread.status ==  "aborting"
+      #     puts "The thread was killed and caught"
+      #     Thread.list.each{|t| Thread.kill(t)}
+      # end
       
-      @data = @mainBucket.first
-      @sort_status = true
+      return $mainBucket.first
     end
 
     #Contract MContracts::TimeLimitNotNil => C::None
-    def watchdog
+    def self.watchdog(timer)
       # task timer
       # ends process if watchdog value is passed
       # watchdog value represented in seconds
       # pre       time_limit
-      sleep @time_limit
-      if @sortingThread.alive?
-        @sortingThread.raise("Thread Death")
+      sleep timer
+      if $sortingThread.alive?
+        $sortingThread.raise("Thread Death")
       end
     end
 
     #Contract C::Num => C::None
-    def build_buckets
+    def self.build_buckets(data, numThreads)
       # main function, goal is to build buckets using data
       # each initial bucket has 2 data points
       # pre       data loaded
       # post      bucket containing data
-      dataclone = @data.clone
-      while dataclone.length > 1 do
-        @mainBucket.push(dataclone.shift(2))
+      dataclone = data.clone
+      sizeOfBuckets = (data.count/numThreads).floor
+      while dataclone.length > sizeOfBuckets-1 do
+        $mainBucket.push(dataclone.shift(sizeOfBuckets))
       end
       
       # Handles the exception if there is an odd number of values in the dataset.
-      if dataclone.length == 1
-        @mainBucket.push(Array.new(1, dataclone.shift))
+      dataclone.each do |val|
+        $mainBucket[numThreads-1].push(val)
       end
+
+
     end
 
-    def thread_handler
+    def self.thread_handler
       # given initial data input, handles thread operations with thread_sort and combine_bucket
       # pre       data
       # post      completed thread operation
       begin
-      while @mainBucket.count != 1
-        # Sort sub buckets in main bucket.
-        threads = Array.new
-        (0..(@mainBucket.count-1)).each do |rank|
-          threads << Thread.new do
-            thread_sort(rank)
+        while $mainBucket.count != 1
+          # Sort sub buckets in main bucket.
+          threads = Array.new
+          (0..($mainBucket.count-1)).each do |rank|
+            threads << Thread.new do
+              thread_sort(rank)
+            end
           end
-        end
-                  
-        # ref http://stackoverflow.com/questions/9095316/handling-exceptions-raised-in-a-ruby-thread
-        threads.each(&:join)
-      
-        # combine sub buckets
-        threads = Array.new
-        @threadBuckets = Array.new()
-        (0..(@mainBucket.count/2).floor-1).each do |rank|
-          threads << Thread.new do
-            combine_bucket(rank)
+                    
+          # ref http://stackoverflow.com/questions/9095316/handling-exceptions-raised-in-a-ruby-thread
+          threads.each(&:join)
+        
+          # combine sub buckets
+          threads = Array.new
+          $threadBuckets = Array.new()
+          (0..($mainBucket.count/2).floor-1).each do |rank|
+            threads << Thread.new do
+              combine_bucket(rank)
+            end
           end
+        
+          threads.each(&:join)
+          if $mainBucket.count % 2 == 1
+            $threadBuckets.push($mainBucket.last)
+          end
+          $mainBucket = $threadBuckets
         end
-      
-        threads.each(&:join)
-        if @mainBucket.count % 2 == 1
-          @threadBuckets.push(@mainBucket.last)
-        end
-        @mainBucket = @threadBuckets
-      end
-      rescue Exception => e
-        puts "EXCEPTION: #{e.inspect}"
-        puts "MESSAGE: #{e.message}"
+      rescue TypeError.new => e
+        # puts "EXCEPTION: #{e.inspect}"
+        # puts "MESSAGE: #{e.message}"
+        raise RuntimeError.new, "Watchdog expired. Sorting has been terminated"
       end
     end
 
     #Contract C::Num => C::None
-    def thread_sort(rank)
+    def self.thread_sort(rank)
       # thread sort operations parsed here
       # Exceptions: thread-based exceptions
       # pre       data
       # post      sorted data
     
       # sort sub bucket.
-      subBucket = @mainBucket[rank]
-      bubble_sort(@mainBucket[rank])
+      subBucket = $mainBucket[rank]
+      bubble_sort($mainBucket[rank])
     
-      # write bucket back to @mainBucket
+      # write bucket back to $mainBucket
       $semaphore.lock
-        @mainBucket[rank] = subBucket
+        $mainBucket[rank] = subBucket
       $semaphore.unlock
     end
     
     #Contract C::Num => C::None
-    def combine_bucket(rank)
+    def self.combine_bucket(rank)
       # combines bucket with another bucket
       # combines data, then performs thread_sort again
       # pre       two sorted buckets
       # post      single sorted bucket
       
       # combine buckets
-      combinedBucket = merge_sorted_arrays(@mainBucket[2*rank], @mainBucket[(2*rank)+1])
+      combinedBucket = merge_sorted_arrays($mainBucket[2*rank], $mainBucket[(2*rank)+1])
       
       # Write combined bucket to bucket list
       $semaphore.lock
-        @threadBuckets.push(combinedBucket)
+        $threadBuckets.push(combinedBucket)
       $semaphore.unlock
     end
     
     #Contract C::ArrayOf[C::Num] => C::Any
-    def bubble_sort(array)
+    def self.bubble_sort(array)
       n = array.length
       loop do
         swapped = false
-            
         (n-1).times do |i|
           if array[i] > array[i+1]
             array[i], array[i+1] = array[i+1], array[i]
@@ -164,14 +161,12 @@ module Multisort
             
         break if not swapped
       end
-
       return array
     end
     
     #Contract C::ArrayOf[C::Num], C::ArrayOf[C::Num] => C::ArrayOf[C::Num]
-    def merge_sorted_arrays(array1, array2)
+    def self.merge_sorted_arrays(array1, array2)
       combinedArray = Array.new
-      
       while (!array1.empty? && !array2.empty?)
         if array1.first <= array2.first
           combinedArray.push(array1.shift)
@@ -179,7 +174,6 @@ module Multisort
           combinedArray.push(array2.shift)
         end
       end
-      
       return combinedArray + array1 + array2
     end
 
--- multisort/test/sorter_test.rb
+++ multisort/test/sorter_test.rb
@@ -9,20 +9,28 @@ class SorterTest < Minitest::Test
   end
 
   def test_sort
-    assert  [1,2] == Multisort.sort([1,2], 20)
-    assert  [10,21,32,4,53,6,71,8,9,10] == Multisort.sort("test/test_files/test_csv.csv", 20)
-    assert  [10.1, 21.2, 32.3, 4.2, 53.3, 6.4, 71.6, 8.8, 9.0, 10.2] == Multisort.sort("test/test_files/test_float_csv.csv", 20)
+    # assert  [1,2] == Multisort.sort([2,1], 20)
+    assert  [1,2,2,3] == Multisort.sort([2,1,2,3], 20, threadNum: 2)
+    assert  [1,2,2,3,3,4,5] == Multisort.sort([2,1,3,4,5,3,2], 20, threadNum: 3)
+    assert  [1,2,2,3,3,4,4,5] == Multisort.sort([2,1,3,4,5,3,2,4], 20, threadNum: 8)
+
+
+    # assert_raises(RuntimeError.new) { Multisort.sort("test/test_files/extra_large_csv.csv", 2) }
+    # assert  [1,2] == Multisort.sort([2,1], 20, threadNum: 4)
+    # assert  [1,2] == Multisort.sort([2,1], 20, threadNum: 8)
+    # assert  [4,6,8,9,10,10,21,32,53,71] == Multisort.sort("test/test_files/test_csv.csv", 20, threadNum: 8)
+    # assert  [10.1, 21.2, 32.3, 4.2, 53.3, 6.4, 71.6, 8.8, 9.0, 10.2] == Multisort.sort("test/test_files/test_float_csv.csv", 20)
     
-    assert [1,2] == Multisort.sort([1,2], 20, threadNum: 4)
-    assert "---\n- 1\n- 2\n" == Multisort.sort([1,2], 20, outputfile: "output.yml")
-    assert "---\n- 1\n- 2\n" == Multisort.sort([1,2], 20, threadNum: 4, outputfile: "output.yml")
+    # assert [1,2] == Multisort.sort([1,2], 20, threadNum: 4)
+    # assert "---\n- 1\n- 2\n" == Multisort.sort([1,2], 20, outputfile: "output.yml")
+    # assert "---\n- 1\n- 2\n" == Multisort.sort([1,2], 20, threadNum: 4, outputfile: "output.yml")
   end
   
   def test_sort_int
     # @ms = Multisort::MSDriver.new
     # @ms.load_from_data([2, 4, 6, 8, 10, 1, 3, 5, 7, 9])
     # assert_equal(true, @ms.data_loaded)
-    # @ms.main_sort
+  # @ms.main_sort
     # assert_equal(true, @ms.data == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
     # @ms.load_from_data([10, 1])
@@ -45,12 +53,9 @@ class SorterTest < Minitest::Test
   end
 
   def test_benchmarks
-    # @ms = Multisort::MSDriver.new
-    # @ms.load_from_file("test/test_files/extra_large_csv.csv")
-    # assert_equal(true, @ms.data_loaded)
-    # timer {@ms.main_sort}
-    # @ms.load_from_file("test/test_files/extra_large_csv.csv")
-    # timer {@ms.bubble_sort(@ms.data)}
+    # timer { Multisort.sort("test/test_files/extra_large_csv.csv", 200)}
+    # data1 = Multisort::IO::load_from_file("test/test_files/extra_large_csv.csv")
+    # timer {Multisort::Sorter::bubble_sort(data1)}
   end
   
   def timer
